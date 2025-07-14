6718ee5014b1ac75f62631196135ba0578886f58
Tehnomant <kadyrgulov.01@mail.ru>
2025-07-14 08:51:17 +0300

    minor chnge :)

--- data/forecasting_framework.py
+++ data/forecasting_framework.py
@@ -8,7 +8,7 @@ from sklearn.metrics import (
 )
 import os
 from forecasting_models import ForecastModel, ClosePriceFM
-from backend.data.data_loader import GoldDataLoader
+from data_loader import GoldDataLoader
 from filter import SGFilter
 
 

2870e2aab69285d360f99f9d0030082a3e2e76fa
Tehnomant <kadyrgulov.01@mail.ru>
2025-07-18 19:21:43 +0300

    updating ml folder

--- data/update_model.py
+++ data/update_model.py
@@ -2,17 +2,26 @@ from forecasting_framework import ForecastFramework
 from forecasting_models import ClosePriceFM
 
 
+model_list = [
+    'xgb_model',
+    'silver_xgb_model'
+]
+
 def update_model():
-    # Create a new framework object
-    fm = ForecastFramework(
-        target_columns=['Close'],
-        forecast_model=ClosePriceFM(),
-        name='xgb_model'
-    )
+    for model in model_list:
+        # Create a new framework object
+        fm = ForecastFramework(
+            target_columns=['Close'],
+            forecast_model=ClosePriceFM(),
+            name=model
+        )
 
-    # Train model
-    fm.train_model()
+        # Train model
+        fm.train_model()
 
-    # Dump model
-    path: str = "xgb_model"
-    fm.dump_model(path=path)
+        # Dump model
+        path: str = model
+        fm.dump_model(path=path)
+
+if __name__ == "__main__":
+    update_model()
